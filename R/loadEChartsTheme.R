05292a3fbddf891dc0ca4d3b714474ca5a738864
Banabanana <liyiheng.scott@gmail.com>
2016-07-30 17:23:31 -0400

    Add a customized theme

--- R/loadEChartsTheme.R
+++ R/loadEChartsTheme.R
@@ -1,10 +1,10 @@
 loadEChartsTheme <- function(theme){
 
   # Check if the value entered for "theme" is valid
-  valid_values <- c("roma", "infographic", "macarons", "vintage", "shine")
+  valid_values <- c("roma", "infographic", "macarons", "vintage", "shine","EmpireLife")
 
   if((theme %in% valid_values) == FALSE){
-    stop("The ECharts theme you specified is invalid. Please check. Valid values include: 'roma', 'infographic', 'macarons', 'vintage' and 'shine'.")
+    stop("The ECharts theme you specified is invalid. Please check. Valid values include: 'roma', 'infographic', 'macarons', 'vintage', 'EmpireLife' and 'shine'.")
   }
 
   to_eval=paste('includeScript(system.file("',
